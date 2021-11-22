7ed2b2dca73cf96a06d7c02bb0a8b0a179e01930
Polina Dudchenko <pv.dudchenko@gmail.com>
2021-11-22 15:40:00 +0100

    modal with hook

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,44 +1,39 @@
 import PropTypes from 'prop-types'
-import { Component } from 'react'
+import { useEffect } from 'react'
 import { createPortal } from 'react-dom'
 import{StyledOverlay, StyledModal} from './Modal.styled'
 
 const modalRoot = document.getElementById('modal-root')
 
-class Modal extends Component {
-  static propTypes = {
+export default function Modal ({onClose, children}) {
+  
+  useEffect(() => {
+    window.addEventListener('keydown', handleEscape)
+
+    return (() => window.removeEventListener('keydown', handleEscape))
+  }, [])
+
+  const handleEscape = (e) => {
+    if (e.code === 'Escape') {
+      
+        onClose()
+    }
+  }
+
+  const handleClose = (e) => {
+    if (e.target === e.currentTarget) {
+      onClose()
+    }
+  }
+
+  return (createPortal(<StyledOverlay onClick={handleClose}>
+        <StyledModal>
+          {children}
+        </StyledModal>
+      </StyledOverlay>, modalRoot) )
+}
+
+Modal.propTypes = {
     children: PropTypes.node.isRequired,
     onClose: PropTypes.func.isRequired,
   }
-
-  componentDidMount() {
-    window.addEventListener('keydown', this.handleEscape)
-  }
-
-  componentWillUnmount() {
-    window.removeEventListener('keydown', this.handleEscape)
-  }
-
-  handleEscape = (e) => {
-    if (e.code === 'Escape') {
-        this.props.onClose()
-    }
-  }
-
-  handleClose = (e) => {
-    if (e.target === e.currentTarget) {
-      this.props.onClose()
-    }
-  }
-
-  render() {
-    return createPortal(<StyledOverlay onClick={this.handleClose}>
-        <StyledModal>
-          {this.props.children}
-        </StyledModal>
-      </StyledOverlay>, modalRoot)
-  }  
-  
-}
-
-export default Modal
